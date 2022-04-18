197c2d534a4482e81b9f8d0b875bdb530eae1e09
Yogesh Selvarajan <yogeshselvarajan@gmail.com>
2022-04-18 17:07:55 +0530

    Login Page with Partial JDBC

--- Login/Login.java
+++ Login/Login.java
@@ -1,175 +1,202 @@
 package Login;
 
-import javax.swing.*;
+
 import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Insets;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class Login
-{
-    // Driver Code
-	public static void main(String args[])
-	{
-		Login pe=new Login();
-	}
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
-    public Login()
-    {
-        Dimension screensize=Toolkit.getDefaultToolkit().getScreenSize();
-        int x=screensize.height;
-        int y=screensize.width;
+public class Login extends JFrame {
 
-        final JFrame f1 =new JFrame("Login Page");
-        f1.setResizable(false);
-        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f1.setBounds((y/2)-250,(x/2)-250,500,500);
-        f1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    private static final long serialVersionUID = 1L;
+    private JTextField textField;
+    private JPasswordField passwordField;
+    private JButton btnNewButton;
+    private JLabel label;
+    private JPanel contentPane;
+    private JButton btnForgetPassword;
 
-        JLabel title=new JLabel("        GYM MANAGEMENT SYSTEM");
-        title.setLayout(null);
-        title.setBounds(35,-200,500,500);
-        title.setFont(new Font("Serif", Font.PLAIN, 25));
-
-        final JTextField usern=new JTextField("User-name");
-        final JPasswordField pass=new JPasswordField("Password");
-        usern.setBounds(90,210,300,50 );
-        pass.setBounds(90,270,300,50);
-
-        ImageIcon icon = new ImageIcon("img/login.jpg");
-        ImageIcon icon2 = new ImageIcon("img/login2.jpg");
-
-        JButton loginButton =new JButton("Login");
-        loginButton.setBounds(160,350,180,60);
-        loginButton.setBorderPainted(false);
-        loginButton.setBorder(null);
-        loginButton.setMargin(new Insets(0, 0, 0, 0));
-        loginButton.setContentAreaFilled(false);
-        loginButton.setIcon(icon2);
-        loginButton.setRolloverIcon(icon2);
-        loginButton.setPressedIcon(icon);
-        loginButton.setDisabledIcon(icon);
-
-        // Lower JPanel
-        JPanel me=new JPanel();
-        JLabel me2=new JLabel("Developed by Yogesh.S and Sree Krishnan.T , B.E CSE , Mepco Schlenk Engineering College");
-        me2.setFont(new Font("Serif", Font.PLAIN, 12));
-        me.setBackground(Color.white);
-        me.setBounds(0,435,500,25);
-        me.add(me2);
-
-        ActionListener al=new ActionListener()
-        {
-            public void actionPerformed(ActionEvent ae)
-            {
-                String user=usern.getText();
-                String passw=String.valueOf(pass.getPassword());
-                /*Username*/
-                if(user.equals("admin") && passw.equals("admin"))
-                {
-                    System.out.println("Login Successful");
-                    f1.dispose();
-                    loginsucc();
-                }
-                else if (user.trim().equals("") || passw.trim().equals(""))
-                    JOptionPane.showMessageDialog(null, "One Or More Fields Are Empty", "Empty Fields", 2);
-                else{
-                    JOptionPane.showMessageDialog(null, "Wrong Username Or Password");
-                    usern.requestFocusInWindow();
-                }
-            }
-        };
-
-
-        loginButton.addActionListener(al);
-
-        usern.addMouseListener(new MouseAdapter()
-        {
-            public void mousePressed(MouseEvent e)
-            {
-                String temp=usern.getText();
-                if(temp.equals("User-name"))
-                    usern.setText("");}
-        });
-
-        pass.addMouseListener(new MouseAdapter()
-        {
-            public void mousePressed(MouseEvent ae)
-            {
-                pass.setText("");
-            }
-
-        });
-
-        usern.addKeyListener(new KeyAdapter()
-        {
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER)
-                {
-                    pass.requestFocusInWindow();
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Login frame = new Login();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
-
-        usern.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                usern.selectAll();
-            }
-
-        });
-
-        pass.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                pass.selectAll();
-            }
-
-        });
-
-        pass.addKeyListener(new KeyAdapter()
-        {
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_ENTER)
-                {
-                    String user=usern.getText();
-                    String passw=String.valueOf(pass.getPassword());
-                    if(user.equals("admin")&&passw.equals("admin"))
-                    {
-                        f1.dispose();
-                        loginsucc();
-                    }
-                    else
-                    {
-                        JOptionPane.showMessageDialog(null, "Wrong Username Or Password");
-                        usern.requestFocusInWindow();
-                    }
-                }
-            }
-        });
-        f1.setLayout(null);
-        f1.add(title);
-        f1.add(usern);
-        f1.add(pass);
-        f1.setVisible(true);
-        f1.add(loginButton);
-        f1.add(me);
     }
 
-    public void loginsucc()
+    /**
+     * Create the frame.
+     */
+    public Login()
     {
-        //Add  code after successful login
-/*
-	add code to run after login is successful
-	*/
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1100,600);
+        setResizable(false);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        contentPane.setBackground((new Color(26, 238, 118)));
 
-        System.out.println("Login Successful Running post Code");
+        JLabel lblNewLabel = new JLabel("Please Login To Proceed ");
+        lblNewLabel.setForeground(Color.BLACK);
+        lblNewLabel.setFont(new Font("Segoe Print", Font.BOLD, 32));
+        lblNewLabel.setBounds(423, 13, 500, 93);
+        lblNewLabel.setForeground(new Color(148,0,211));
+        contentPane.add(lblNewLabel);
+
+        textField = new JTextField();
+        textField.setFont(new Font("Segoe Print", Font.BOLD, 20));
+        textField.setBounds(481, 170, 281, 40);
+        contentPane.add(textField);
+        textField.setColumns(10);
+
+        passwordField = new JPasswordField();
+        passwordField.setFont(new Font("Sogoe Print", Font.BOLD, 20));
+        passwordField.setBounds(481, 286, 281, 40);
+        contentPane.add(passwordField);
+
+        JLabel lblUsername = new JLabel("User Name :");
+        lblUsername.setBackground(Color.BLACK);
+        lblUsername.setForeground(Color.BLACK);
+        lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 25));
+        lblUsername.setBounds(250, 166, 193, 52);
+        lblUsername.setForeground(new Color(178,34,34));
+        contentPane.add(lblUsername);
+
+        JLabel lblPassword = new JLabel("Password :");
+        lblPassword.setForeground(Color.BLACK);
+        lblPassword.setBackground(Color.CYAN);
+        lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 25));
+        lblPassword.setBounds(250, 286, 193, 52);
+        lblPassword.setForeground(new Color(178,34,34));
+        contentPane.add(lblPassword);
+
+        btnNewButton = new JButton("Login");
+        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        btnNewButton.setForeground(new Color(174,34,34));
+        btnNewButton.setBackground(new Color(0,191,255));
+        btnNewButton.setBounds(545, 392, 162, 73);
+        btnNewButton.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                String userName = textField.getText();
+                String password = String.valueOf(passwordField.getPassword());
+                try {
+                    Connection connection = (Connection) DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl",
+                            "system", "orcl");
+
+                    PreparedStatement st = (PreparedStatement) connection
+                            .prepareStatement("Select USER_NAME, PASSWORD from TBLLOGIN where USER_NAME=? and PASSWORD=?");
+
+                    st.setString(1, userName);
+                    st.setString(2, password);
+                    ResultSet rs = st.executeQuery();
+                    if (rs.next()) {
+                        dispose();
+                        /*
+                        UserDashBoard ah = new UserDashBoard(userName);
+                        ah.setTitle("Welcome");
+                        ah.setVisible(true);
+                        */
+                        JOptionPane.showMessageDialog(btnNewButton, "You have successfully logged in");
+                    }
+                    else if (userName.trim().equals("") || password.trim().equals(""))
+                        JOptionPane.showMessageDialog(null, "One Or More Fields Are Empty", "Empty Fields", 2);
+                    else
+                        JOptionPane.showMessageDialog(btnNewButton, "Wrong Username & Password");
+                } catch (SQLException sqlException) {
+                    sqlException.printStackTrace();
+                }
+            }
+        });
+
+        contentPane.add(btnNewButton);
+
+        btnForgetPassword = new JButton("Forget Password");
+        btnForgetPassword.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        btnForgetPassword.setBounds(320, 392, 190, 73);
+        btnForgetPassword.setForeground(new Color(174,34,34));
+        btnForgetPassword.setBackground(new Color(0,191,255));
+        btnForgetPassword.addActionListener( e ->
+        {
+            dispose();
+         /*   ForgetPassword ob = new ForgetPassword();
+            ob.main(null);
+          */
+        });
+        contentPane.add(btnForgetPassword);
+
+        JButton btnAdminLogin = new JButton("Admin Login");
+        btnAdminLogin.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        btnAdminLogin.setBounds(40, 392, 250, 73);
+        btnAdminLogin.setForeground(new Color(174,34,34));
+        btnAdminLogin.setBackground(new Color(0,191,255));
+        btnAdminLogin.addActionListener(e ->
+        {
+            String userName = textField.getText();
+            String password = String.valueOf(passwordField.getPassword());
+            try {
+                Connection connection = (Connection) DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl",
+                        "system", "orcl");
+
+                PreparedStatement st = (PreparedStatement) connection
+                        .prepareStatement("Select USER_NAME, PASSWORD from TBLLOGIN where USER_NAME=? and PASSWORD=?");
+
+                st.setString(1, userName);
+                st.setString(2, password);
+                ResultSet rs = st.executeQuery();
+                if (rs.next()) {
+                    dispose();
+                    /*
+                    AdminDashBoard ah = new AdminDashBoard();
+                    ah.main(null);
+                    */
+                    JOptionPane.showMessageDialog(btnNewButton, "You have successfully logged in");
+                }
+                else if (userName.trim().equals("") || password.trim().equals(""))
+                    JOptionPane.showMessageDialog(null, "One Or More Fields Are Empty", "Empty Fields", 2);
+                else
+                    JOptionPane.showMessageDialog(btnNewButton, "Wrong Admin Credentials !");
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
+        });
+        contentPane.add(btnAdminLogin);
+
+        JButton btnRegister = new JButton("New Account");
+        btnRegister.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        btnRegister.setBounds(750, 392, 250, 73);
+        btnRegister.setForeground(new Color(174,34,34));
+        btnRegister.setBackground(new Color(0,191,255));
+        btnRegister.addActionListener(e ->
+        {
+            dispose();
+            /*
+            UsernameAvailablility obj = new UsernameAvailablility();
+            obj.main();
+           */
+        });
+        contentPane.add(btnRegister);
+        label = new JLabel("");
+        label.setBounds(0, 0, 1608, 562);
+        contentPane.add(label);
     }
 }

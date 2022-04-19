54a130e3d965bad3a3b18298a44df36b6856629e
Yogesh Selvarajan <yogeshselvarajan@gmail.com>
2022-04-19 10:56:47 +0530

    Added DBTest.java

--- DBTest.java
+++ DBTest.java
@@ -1,23 +1,56 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class DBTest
-{
-    public static Connection connect()
-        {
-            Connection con=null;
-            try {
-                Class.forName("");
-                con=DriverManager.getConnection("jdbc:mysql://localhost:3306/database name?","userName","password");
-                // JOptionPane.showConfirmDialog(null,"connect to data base");
+public class DBTest {
 
-            } catch (Exception e) {
-                System.out.println("inter.DBConnect.connect()");
-                JOptionPane.showMessageDialog(null, e);
-            }
-            return con;
+    public static void main(String args[]) {
+
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            System.out.println("Oops! Got a SQL error: " + e.getMessage());
+            System.exit(1);
         }
 
-    }
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "system", "orcl");
 
+            stmt = conn.createStatement();
+            rset = stmt
+                    .executeQuery("select 'Hello '||USER||'!' result from dual");
+            while (rset.next())
+                System.out.println(rset.getString(1));
+            rset.close();
+            rset = null;
+            stmt.close();
+            stmt = null;
+            conn.close();
+            conn = null;
+        } catch (SQLException e) {
+            System.out.println("A SQL error: " + e.getMessage());
+        } finally {
+            if (rset != null)
+                try {
+                    rset.close();
+                } catch (SQLException ignore) {
+                }
+            if (stmt != null)
+                try {
+                    stmt.close();
+                } catch (SQLException ignore) {
+                }
+            if (conn != null)
+                try {
+                    conn.close();
+                } catch (SQLException ignore) {
+                }
+        }
+    }
+}

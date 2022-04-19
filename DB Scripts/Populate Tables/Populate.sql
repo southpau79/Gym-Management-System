d36373e71857d2a53a7f7864f8b47b0dc08e7184
Yogesh Virtual Machine <yogeshselvarajan@gmail.com>
2022-04-19 22:32:56 +0530

    Added Populate.sql for Populating Table with Values

---
+++ DB Scripts/Populate Tables/Populate.sql
@@ -0,0 +1,312 @@
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (1, 'Sarah', 'D', 'Pastor', 63, 'F', 'nova_stiedema@gmail.com', 2333, 'Dane Street', 'Framingham', 17021,
+        'Massachusetts(MA)');
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (2, 'Darla', 'C', 'Waring', 53, 'F', 'flavio2007@gmail.com', 1886, 'Pin Oak Drive', 'Davenport', 52803,
+        'Iowa(IA)');
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (3, 'Tyson', 'S', 'Kelly', 41, 'M', 'chyna_blic2@yahoo.com', 1550, 'Ash Street', 'Dallas', 75225, 'Texas(TX)');
+
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (4, 'Christine', 'P', 'Ferreira', 38, 'F', 'prince1994@hotmail.com', '4100', 'Bagwell Avenue', 'Gainesville',
+        32601, 'Florida(FL)');
+
+insert into TBLUSERS(USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                     PINCODE, STATE)
+values (5, 'Joseph', 'G', 'Perry', 24, 'M', 'ena_harbe7@hotmail.com', 3228, ' Kildeer Drive', 'Williamsburg', 23185,
+        'Virginia(VA)');
+
+insert into TBLUSERS(USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                     PINCODE, STATE)
+values (6, 'Tiffany', 'M', 'Lacy', 27, 'F', 'ollie1973@yahoo.com', 1421, 'Lochmere Lane', 'Stamford', 06995,
+        'Connecticut(CT)');
+
+insert into TBLUSERS(USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                     PINCODE, STATE)
+values (7, 'Adam', 'S', 'Ramsey', 34, 'M', 'emanuel_bee0@hotmail.com', 1170, 'Laurel Lee', 'Saint Paul', 55106,
+        ' Minnesota(MN)');
+
+insert into TBLUSERS(USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                     PINCODE, STATE)
+values (8, 'Richard', 'D', 'Treino', 20, 'M', 'julio2009@hotmail.com', 3107, 'Fincham Road', 'Ontario', 91761,
+        'California(CA)');
+
+insert into TBLUSERS(USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                     PINCODE, STATE)
+values (9, 'Nicholas', 'S', 'Turner', 21, 'M', 'wyatt.labad@yahoo.com', 4999, 'Woodstock Drive', 'San Bernardino',
+        92401, 'California(CA)');
+
+insert into TBLUSERS(USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                     PINCODE, STATE)
+values (10, 'Rodney', 'C', 'Frazier', 28, 'M', 'elmira_fun7@hotmail.com', 3514, 'Par Drive', 'San Barbara', 93101,
+        'California(CA)');
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (11, 'Donald', 'B', 'Grunt', 25, 'M', 'ellie_carte8@yahoo.com', 32, 'Newton Street', 'Saint Cloud', 56301,
+        'Minnesota(MN)');
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (12, 'Ann', 'J', 'Brown', 24, 'F', 'hilton2007@yahoo.com', 4132, 'Turkey Pen Lane', 'Dothan', 36301,
+        'Alabama(AL)');
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (13, 'Deborah', 'D', 'Thornton', 37, 'F', 'tremayne2004@gmail.com', 3873, 'Amethyst Drive', 'Lansing', 48906,
+        'Michigan(MI)');
+
+insert into TBLUSERS (USER_ID, FIRST_NAME, MIDDLE_NAME, LAST_NAME, AGE, GENDER, EMAIL_ID, DOOR_NO, STR_NAME, CITY,
+                      PINCODE, STATE)
+values (14, 'Kimberly', 'E', 'Brown', 41, 'F', 'makenna2001@hotmail.com', 1289, ' Hide A Way Road', 'Santa Clara',
+        95054, 'California(CA)');
+
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (6195690210, 1);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (2293255079, 2);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (3219472819, 3);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (4195485681, 4);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (3175389244, 5);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (7159065121, 6);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (2142385137, 7);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (8015090269, 8);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (8034504306, 9);
+insert into TBLUSER_PHONE_NUMBER (PHONE_NUMBER, USER_ID)
+values (2063139197, 10);
+insert into TBLUSER_PHONE_NUMBER(PHONE_NUMBER, USER_ID)
+values (4084605605, 11);
+insert into TBLUSER_PHONE_NUMBER(PHONE_NUMBER, USER_ID)
+values (8186077797, 12);
+insert into TBLUSER_PHONE_NUMBER(PHONE_NUMBER, USER_ID)
+values (2133139572, 13);
+insert into TBLUSER_PHONE_NUMBER(PHONE_NUMBER, USER_ID)
+values (7345643502, 14);
+
+
+insert into TBLROLES (ROLE_ID, ROLE_NAME, ROLE_DESCRIPTION)
+values (1, 'Bodybuilding coach', 'A bodybuilding coach works with individuals, athletes, ' ||
+                                 'sports teams and competitive bodybuilders to help them achieve their fitness and physique goals. ' ||
+                                 'Bodybuilding coaches design weightlifting routines using scientific research ' ||
+                                 'to ensure maximum effectiveness.');
+insert into TBLROLES (ROLE_ID, ROLE_NAME, ROLE_DESCRIPTION)
+values (2, 'Dietitian', ' A dietitian is knowledgeable about the dietary needs of anyone' ||
+                        ' who makes fitness a large part of their lives. ' ||
+                        'They can inform and assist clients in developing meal plans based on their ' ||
+                        'caloric output and recommend vitamins and supplements.' ||
+                        ' Their work helps improve performance and manage weight so their clients can ' ||
+                        'live a healthy lifestyle that supports their fitness goals.');
+
+insert into TBLROLES (ROLE_ID, ROLE_NAME, ROLE_DESCRIPTION)
+values (3, 'Equipment Technician', 'A fitness equipment technician repairs and replaces gym equipment. ' ||
+                                   'They work on treadmills, exercise bikes, ellipticals and universal weightlifting machines. ' ||
+                                   'Fitness equipment technicians can service equipment in gyms, physical therapists'' offices, ' ||
+                                   'apartment complexes, office buildings or individual homes.');
+
+insert into TBLROLES (ROLE_ID, ROLE_NAME, ROLE_DESCRIPTION)
+values (4, 'DB Administrator', 'The responsibilities of DBA are as follows −' ||
+                               'Makes the decision concerning the content of the database.' ||
+                               'Plans the storage structure and access strategy.' ||
+                               'Provides the support to the users.' ||
+                               'Defines the security and integrity checks.');
+
+insert into TBLROLES (ROLE_ID, ROLE_NAME, ROLE_DESCRIPTION)
+values (5, 'Member', 'A person who has subscribed  to the Gym Services for a particular need.');
+
+insert into TBLROLES(ROLE_ID, ROLE_NAME, ROLE_DESCRIPTION)
+values (6, 'Instructor', 'A  fitness instructor, also known as a personal trainer, ' ||
+                         'provides training and instruction of fitness programs to individuals and groups. ' ||
+                         'The fitness instructor engages clients in exercise routines and weight loss programs, ' ||
+                         'and helps them to reach their individual goals. ');
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (1, 'sarahh', 'GdKpGdlh', 5);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (2, 'darlac', 'vyHiSqht', 5);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (3, 'tysons', 'XedhInsT', 5);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (4, 'christinep', 'zVtehsyt', 4);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (5, 'josephg', 'BwKNRrez', 4);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (6, 'tiffanyf', 'RkoXAZxZ', 1);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (7, 'adams', 'jgnvdwKD', 2);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (8, 'richardd', 'iNjsXNkR', 1);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (9, 'nicholass', 'ceYqFqNE', 3);
+
+insert into TBLLOGIN (USER_ID, USER_NAME, PASSWORD, ROLE_ID)
+values (10, 'rodneyc', 'dYimkLbG', 3);
+
+insert into TBLPROMOTIONS (PROMOTION_ID, USER_ID, PROMOTION_NAME, PROMOTION_DETAILS)
+values (829047, 4, 'Renewal Offer', 'Renew your existing membership plan today and enjoy amazing benefits.');
+
+insert into TBLPROMOTIONS (PROMOTION_ID, USER_ID, PROMOTION_NAME, PROMOTION_DETAILS)
+values (827829, 4, 'Joining Offer',
+        'There is something for everyone! The Gym offers various membership options to suit your requirement ' ||
+        'and helps you to achieve your fitness goals. Let''s do this!');
+
+insert into TBLPAYMENT (PAYMENT_ID, USER_ID, PAYMENT_TIME)
+values (33, 3, CURRENT_TIMESTAMP);
+
+insert into TBLPAYMENT (PAYMENT_ID, USER_ID, PAYMENT_TIME)
+values (19, 1, CURRENT_TIMESTAMP);
+
+insert into TBLPAYMENT (PAYMENT_ID, USER_ID, PAYMENT_TIME)
+values (65, 2, CURRENT_TIMESTAMP);
+
+insert into TBLMEMBERSHIP_TYPE (TYPE_ID, TYPE_NAME, AMOUNT)
+values (1, 'Monthly', 700);
+
+insert into TBLMEMBERSHIP_TYPE (TYPE_ID, TYPE_NAME, AMOUNT)
+values (2, 'Quarterly', 2500);
+
+insert into TBLMEMBERSHIP_TYPE (TYPE_ID, TYPE_NAME, AMOUNT)
+values (3, 'Semi-Yearly', 3800);
+
+insert into TBLMEMBERSHIP_TYPE (TYPE_ID, TYPE_NAME, AMOUNT)
+values (4, 'Semi-Yearly', 8000);
+
+insert into TBLMEMBERSHIP (MEMBERSHIP_ID, USER_ID, JOINING_DATE, END_DATE, TYPE_ID)
+values (565361, 3, '17-04-2022', '17-05-2022', 1);
+
+insert into TBLMEMBERSHIP (MEMBERSHIP_ID, USER_ID, JOINING_DATE, END_DATE, TYPE_ID)
+values (350623, 1, '17-04-2022', '17-05-2022', 1);
+
+insert into TBLMEMBERSHIP (MEMBERSHIP_ID, USER_ID, JOINING_DATE, END_DATE, TYPE_ID)
+values (947001, 2, '17-04-2022', '17-10-2022', 3);
+
+
+INSERT INTO TBLINSTRUCTOR(INSTRUCTOR_ID, USER_ID)
+values (245191, 11);
+
+INSERT INTO TBLINSTRUCTOR(INSTRUCTOR_ID, USER_ID)
+values (245192, 12);
+
+INSERT INTO TBLINSTRUCTOR(INSTRUCTOR_ID, USER_ID)
+values (245193, 13);
+
+INSERT INTO TBLINSTRUCTOR(INSTRUCTOR_ID, USER_ID)
+values (245194, 14);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (1, 245191, 15);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (2, 245192, 30);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (3, 245193, 45);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (4, 245194, 60);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (5, 245191, 15);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (6, 245192, 30);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (7, 245193, 25);
+
+insert into TBLWORKOUT_PLAN (PLAN_ID, INSTUCTOR_ID, WORKOUT_DURATION)
+values (8, 245194, 90);
+
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (1, 1, 'Squats', 20, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (2, 1, 'Plie Squats', 20, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (3, 1, 'Lunge Kicks', 20, 'Per Side');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (4, 1, 'Leg Swings', 20, 'Per Side');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (5, 1, 'Donkey Kicks', 20, 'Per Side');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (6, 1, 'Fire Hydrants', 20, 'Per Side');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (7, 1, 'Circle Donkey Kicks', 20, 'Per Side');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (8, 1, 'Hip Lifts', 30, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (9, 1, 'Alternating Split Jumps', 30, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (10, 1, 'Sliders', 30, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (11, 5, 'Narrow-Stance Goblet Squat', 15, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (12, 5, 'Single-Arm Bent-Over Row', 10, 'Switch Sides after 10 counts');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (13, 5, 'Single-Arm Chest Press', 10, 'Switch Sides after 10 counts');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (14, 5, 'Dumbbell Swing', 15, '');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (15, 2, 'MedBall V-Up', 8, '3 sets then 60 secs rest');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (16, 2, 'Side Plank with a crunch', 1, '3 sets then 60 secs rest');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (17, 2, 'Flutter Kicks', 10, '3 sets then 60 secs rest');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (18, 2, 'Shoulder Taps', 10, '3 sets then 60 secs rest');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (19, 2, 'Pallof Press', 10, '3 sets then 60 secs rest');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (20, 2, 'Swiss Ball Roll Outs', 5, '3 sets then 60 secs rest');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (21, 6, 'Hip Thrusts', 10, '3 sets');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (22, 6, 'Goblet Squats', 10, '3 sets');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (23, 6, 'Thrusters', 15, '3 sets');
+
+insert into TBLWORKOUT_DETAILS (SNO, PLAN_ID, EXERCISE_NAME, COUNTS, COMMENTS)
+values (24, 6, 'Renegade Rows', 10, '3 sets');
